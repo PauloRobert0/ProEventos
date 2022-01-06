c8faff89a62ad608fda1bac6b78824a6dbb71ed2
PauloRobert0 <pauloebine@hotmail.com>
2022-01-06 10:36:03 -0300

    Layout Responsivo + Forms

--- Front/ProEventos-app/src/app/app.module.ts
+++ Front/ProEventos-app/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContatosComponent } from './components/contatos/contatos.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+import { PerfilComponent } from './components/user/perfil/perfil.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
 import { TituloComponent } from './shared/Titulo/Titulo.component';
@@ -25,6 +25,11 @@ import { NavComponent } from './shared/nav/nav.component';
 import { EventoService } from './services/Evento.service';
 
 import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
+import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
+import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
+import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
 
 
 
@@ -39,6 +44,11 @@ import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
     TituloComponent,
     NavComponent,
     DateTimeFormatPipe,
+    EventoDetalheComponent,
+    EventoListaComponent,
+    UserComponent,
+    LoginComponent,
+    RegistrationComponent,
    ],
   imports: [
     BrowserModule,
